877bbee410d55026976451fcd202e2e2e94e44f8
Thomas Francis Shannon <Thomas@Thomas-MacBook-Pro.local>
2015-10-20 16:58:53 -0700

    testing cancel class

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 $(document).ready(function(){
 	$('#section1').on('click',function(){
 	$('#content1').toggleClass("displayBlock");
+		
+
+
 	});
+
 	$('#section2').on('click',function(){
 	$('#content2').toggleClass("displayBlock");
+
 	});
+
 	$('#section3').on('click',function(){
 	$('#content3').toggleClass("displayBlock");
+
 	});
+
 });
+
+
+
+	//if ($('#content1').on('click')){
+	//	$('#content2').toggleClass("cancel") && $('#content3').toggleClass("cancel");
