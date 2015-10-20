33ebd150c1410760ea69a78fe5687f0282c1ffb9
Thomas Francis Shannon <Thomas@Thomas-MacBook-Pro.local>
2015-10-20 16:13:36 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('#section1').on('click',function(){
+	$('#content1').toggleClass("displayBlock");
+	});
+	$('#section2').on('click',function(){
+	$('#content2').toggleClass("displayBlock");
+	});
+	$('#section3').on('click',function(){
+	$('#content3').toggleClass("displayBlock");
+	});
+});
