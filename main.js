135d66c1bc4ba36dc9f3571fe7fe9ba987c894eb
Thomas Francis Shannon <Thomas@Thomas-MacBook-Pro.local>
2015-10-20 21:31:01 -0700

    cleared the other contents

--- main.js
+++ main.js
@@ -1,24 +1,30 @@
 $(document).ready(function(){
 	$('#section1').on('click',function(){
 	$('#content1').toggleClass("displayBlock");
-		
-
+	clear('#content2');
+	clear('#content3');
 
 	});
 
 	$('#section2').on('click',function(){
 	$('#content2').toggleClass("displayBlock");
-
+	clear('#content1');
+	clear('#content3');
 	});
 
 	$('#section3').on('click',function(){
 	$('#content3').toggleClass("displayBlock");
-
+	clear('#content1');
+	clear('#content2');
 	});
 
 });
-
-
+	var clear= function(x) {
+		$(x).removeClass("displayBlock");
+	}
+	
+	
+		
 
 	//if ($('#content1').on('click')){
 	//	$('#content2').toggleClass("cancel") && $('#content3').toggleClass("cancel");
